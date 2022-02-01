0cb3c3e23422e1d75309842b9c35435d14ce1c0e
KevOub <kjo018@latech.edu>
2022-02-01 09:02:35 -0600

    pls work now

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 wget https://github.com/KevOub/bonk/blob/master/bonk?raw=true -O /usr/sbin/bonk
 chmod u+x /usr/sbin/bonk
 echo "Installed the bonk"
