ccf0f26f1d333e97bfdd1920a39693c155d443e2
KevOub <kjo018@latech.edu>
2022-02-03 15:01:22 -0600

    fixed typo

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/KevOub/bonk/blob/master/bonk?raw=true -O /usr/sbin/bonk
+wget -O /usr/sbin/bonk https://github.com/KevOub/bonk/blob/master/bonk?raw=true 
 chmod u+x /usr/sbin/bonk
 echo "Installed the bonk"
