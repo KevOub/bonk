bea05057846bcac78c7d00703b85279dbf66fcc6
KevOub <kjo018@latech.edu>
2022-04-13 11:05:25 -0500

    Very difficult to speed up rule addition but easy to make it more readable

--- cmd/bonk2.go
+++ cmd/bonk2.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"embed"
+	_ "embed"
 	"fmt"
 
 	"github.com/KevOub/bonk/pkg/bonk"
 )
 
 func test(a bonk.AuditMessageBonk, b bonk.Config) {
-	fmt.Printf("%s\n", a.AuditID)
+	fmt.Printf("%+v\n", a)
 }
+
+// EMBEDDING LOGIC
+//go:embed embed/config.json embed/good.rules
+var content embed.FS
 
 func main() {
 
 	var parser bonk.AuditMessageBonk
 	var config bonk.Config
 
+	// f, _ := content.Open("embed/config.json")
+	// data, _ := ioutil.ReadFile("embed/config.json")
+
+	// take the config.json and set the config struct accordingly
+	data, err := content.ReadFile("embed/config.json")
+	if err != nil {
+		panic(err)
+	}
+	config.LoadFromIO(data)
+
+	// Here is how to embed good.rules into the struct
+	defaultRules, err := content.ReadFile("embed/good.rules")
+	if err != nil {
+		panic(err)
+	}
+	config.EmbedRules(defaultRules)
+
+	// Finally, specify operation
+	config.Operation = bonk.LOCKANDLOAD
+	config.DontEmbedRules = false
+	config.Verbose = true
+	// and enter the infinite loop
 	bonk.StreamAudit(parser, config, test)
 }
